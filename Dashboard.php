33fd58f49fadb3ccf34240e96bef3251017c557b
EphraimMalope <ephraimmalope4@gmail.com>
2024-04-16 13:04:34 +0200

    updated

--- Dashboard.php
+++ Dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration was Successful</title>
+    <title>Registration was Successful feel free</title>
 </head>
 <body>
     <h1>Congratulations!</h1>
--- Dashboard1.php
+++ Dashboard1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in Successful</title>
+    <title>Log in was Successful</title>
 </head>
 <body>
     <h1>YOU LOGGED IN!</h1>
