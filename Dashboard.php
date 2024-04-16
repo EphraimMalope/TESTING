87309d43a1c094af8af12e0e99122a91a0dd1871
EphraimMalope <ephraimmalope4@gmail.com>
2024-04-16 13:42:59 +0200

    update

--- Dashboard.php
+++ Dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration was Successful feel free to interact</title>
+    <title>Registration was Successful feel free to interact with everyone</title>
 </head>
 <body>
     <h1>Congratulations!</h1>
